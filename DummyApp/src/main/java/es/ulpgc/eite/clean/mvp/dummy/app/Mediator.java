8b7356eecd3d13473a8ec76b38135a3022a603b0
Luis Pacheco <pacheco.lorenzo.luis@gmail.com>
2017-02-14 12:24:57 +0000

    Initial commit

---
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/dummy/app/Mediator.java
@@ -0,0 +1,10 @@
+package es.ulpgc.eite.clean.mvp.dummy.app;
+
+import es.ulpgc.eite.clean.mvp.dummy.dummy.Dummy;
+import es.ulpgc.eite.clean.mvp.dummy.hello.Hello;
+
+public interface Mediator {
+  void startingDummyScreen(Dummy.ToDummy presenter);
+
+  void startingHelloScreen(Hello.ToHello presenter);
+}
